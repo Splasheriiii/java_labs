7bb3be51d828a357cdb38930cff9dc02d204b634
Splasheri <splasheri@yahoo.com>
2024-11-04 22:40:08 +0500

    init

---
+++ src/main/java/ru/urfu/lr7/dao/StudentRepository.java
@@ -0,0 +1,9 @@
+package ru.urfu.lr7.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.urfu.lr7.entity.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+}
